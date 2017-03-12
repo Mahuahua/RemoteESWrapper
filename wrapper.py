ae5399d6c2e6fb91d588b973ded0d9ded4bc80dd
Ray Wang <mahuahua.mushroom@gmail.com>
2017-03-12 18:49:00 -0400

    back and forth

--- wrapper.py
+++ wrapper.py
@@ -41,9 +41,9 @@ class ElasticSearch(Resource):
         print(payload)
         response = requests.request("POST", url, data=payload)
 
-        print(jsonify(response))
+        print(response.json())
 
-        return jsonify(response), 200
+        return response.json(), 200
 
     def delete(self):
         return '', 405
